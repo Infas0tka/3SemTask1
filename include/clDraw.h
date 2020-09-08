ef41e5d9212f727f14fab45cf9f7ac72085d8ab5
Infas0tka <65242283+Infas0tka@users.noreply.github.com>
2020-09-08 21:23:33 +0300

    Add files via upload

---
+++ include/clDraw.h
@@ -0,0 +1,12 @@
+#ifndef CLDRAW_H
+#define CLDRAW_H
+#include <graphics.h>
+
+
+class clDraw
+{
+    public:
+        void virtual show() = 0;
+};
+
+#endif // CLDRAW_H
---
+++ include/clNode.h
@@ -0,0 +1,25 @@
+#ifndef CLNODE_H
+#define CLNODE_H
+
+#include <cstdlib>
+#include "clCircle.h"
+
+
+class clNode : public clCircle
+{
+    public:
+        clNode();
+        clNode(int x0, int y0, int num);
+        virtual ~clNode();
+
+        //char* Getstr() { return *str; }
+        void Setstr(int val) { itoa(val, str, 10); }
+        void show();
+
+    protected:
+
+    private:
+        char *str;
+};
+
+#endif // CLNODE_H
