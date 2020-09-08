a98fed6e970ecd02785f04fef892e33e3ccd711b
Infas0tka <65242283+Infas0tka@users.noreply.github.com>
2020-09-08 20:34:56 +0300

    Create clCircle.cpp

---
+++ src/clCircle.cpp
@@ -0,0 +1,24 @@
+#include "clCircle.h"
+#include <iostream>
+
+using namespace std;
+
+clCircle::clCircle()
+{
+    cout << "clCircle::clCircle()" << endl;
+}
+
+clCircle::clCircle(int x0, int y0):x(x0), y(y0)
+{
+    cout << "clCircle::clCircle(int x0, int y0)" << endl;
+}
+
+clCircle::~clCircle()
+{
+    cout << "clCircle::~clCircle()" << endl;
+}
+
+void clCircle::show()
+{
+    circle(x, y, r);
+}
